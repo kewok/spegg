4bd05d2a74caa1827730204a55f79214f528c95e
kewok <kwokamoto@gmail.com>
2017-08-05 15:03:41 -0500

    Removed unused female nesting functions from GPU version of physiologically structured model.

--- Examples/PhysiologicalStructure_LifeHistoryEvolution/src/Fish_Parents.cu
+++ Examples/PhysiologicalStructure_LifeHistoryEvolution/src/Fish_Parents.cu
@@ -68,27 +68,3 @@ void Fish_Parents::determine_probability_individual_becomes_male_parent()
                	 M_reproductive_probability);
 	}
 
-void Fish_Parents::female_nesting_success()
-	{
-	/* whether the female is able to nest; this effectively governs female reproductive success */
-	/* NB later implementations can vary the success of the female getting a nest to depend on her relative body size etc... */
-	reduce_by_key_with_zeros(deme, will_reproduceF, reproductive_females_per_deme, size, Num_Demes);
-
-	thrust::device_vector<float> reproductive_females_per_deme_float(Num_Demes); // figure out reproductive_females
-
-	thrust::copy(reproductive_females_per_deme.begin(), reproductive_females_per_deme.end(), reproductive_females_per_deme_float.begin());
-
-	float *total_female_population_size_ptr = raw_pointer_cast(&reproductive_females_per_deme_float[0]);
-	float *nest_ceiling_ptr = raw_pointer_cast(demeParameters->get_vector_ptr("nest_ceiling"));
-	int *males_per_deme_ptr = raw_pointer_cast(&reproductive_males_per_deme[0]);
-
-	determine_female_nesting_success_functor determine_female_nesting_success(nest_ceiling_ptr, total_female_population_size_ptr, males_per_deme_ptr) ;
-
-	thrust::device_vector<float> rand(size);
-	float *rand_ptr = raw_pointer_cast(&rand[0]);
-	curandGenerateUniform(gen, rand_ptr, size);
-	
-	thrust::for_each(thrust::make_zip_iterator(thrust::make_tuple(will_reproduceF.begin(), deme.begin(), rand.begin())), 
-			thrust::make_zip_iterator(thrust::make_tuple(will_reproduceF.begin() + size, deme.begin() + size, rand.begin() + size)), 
-			determine_female_nesting_success);
-	}
